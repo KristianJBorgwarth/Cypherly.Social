b15f895cab50ee7c10a39ba47ce94f89aadeb0f0
Kristian <10348902@pm.me>
2025-08-30 18:03:19 +0200

    removed identity downstream call

--- Social.Test.Integration/UserProfileTest/QueryTest/GetUserProfileTest/GetUserProfileQueryHandlerTest.cs
+++ Social.Test.Integration/UserProfileTest/QueryTest/GetUserProfileTest/GetUserProfileQueryHandlerTest.cs
@@ -16,17 +16,15 @@ namespace Social.Test.Integration.UserProfileTest.QueryTest.GetUserProfileTest;
 public class GetUserProfileQueryHandlerTest : IntegrationTestBase
 {
     private readonly GetUserProfileQueryHandler _sut;
-    private readonly IConnectionIdProvider _connectionIdProvider;
-
+    
     public GetUserProfileQueryHandlerTest(IntegrationTestFactory<Program, SocialDbContext> factory) : base(factory)
     {
         var scope = factory.Services.CreateScope();
         var repo = scope.ServiceProvider.GetRequiredService<IUserProfileRepository>();
         var profilePictureService = scope.ServiceProvider.GetRequiredService<IProfilePictureService>();
-        _connectionIdProvider = A.Fake<IConnectionIdProvider>();
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<GetUserProfileQueryHandler>>();
 
-        _sut = new(repo, profilePictureService, _connectionIdProvider, logger);
+        _sut = new(repo, profilePictureService, logger);
     }
 
     [Fact]
@@ -38,8 +36,6 @@ public class GetUserProfileQueryHandlerTest : IntegrationTestBase
         Db.UserProfile.Add(userProfile);
         await Db.SaveChangesAsync();
 
-        A.CallTo(()=> _connectionIdProvider.GetConnectionIdsByUser(userProfile.Id, A<CancellationToken>._))
-            .Returns([Guid.NewGuid(), exclusiveConnectionId]);
 
         var query = new GetUserProfileQuery { TenantId = userProfile.Id, ExclusiveConnectionId = exclusiveConnectionId};
 
@@ -49,8 +45,6 @@ public class GetUserProfileQueryHandlerTest : IntegrationTestBase
         // Assert
         result.Success.Should().BeTrue();
         result.Value.Should().NotBeNull();
-        result.Value.ConnectionIds.Should().HaveCount(1);
-        result.Value.ConnectionIds.Should().NotContain(exclusiveConnectionId);
     }
 
     [Fact]
