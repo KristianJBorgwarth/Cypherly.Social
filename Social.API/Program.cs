ae0ec55e7605cdfa30bc5ce29efb68dbcef3e9b9
Kristian <10348902@pm.me>
2025-12-02 19:43:50 +0100

    fix: mixxing logging setup

--- Social.API/Program.cs
+++ Social.API/Program.cs
@@ -24,6 +24,7 @@ if (env.IsDevelopment())
     configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 }
 
+builder.AddLogging();
 builder.Services.AddObservability(configuration);
 
 builder.Services.AddDomain();
