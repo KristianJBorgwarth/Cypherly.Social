e86babf66cd00391e85a38b181dafd3f0afd6f44
Kristian Borgwarth <10348902@pm.me>
2026-05-16 21:05:04 +0200

    feat(user_profile): add avatar

--- Social.Domain/Entities/Avatar.cs
+++ Social.Domain/Entities/Avatar.cs
@@ -6,14 +6,14 @@ namespace Social.Domain.Entities;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 public sealed class Avatar : Entity
 {
-    public Guid UserId { get; private set; }
+    public Guid UserProfileId { get; private set; }
     public ETag Etag { get; private set; }
 
     public Avatar() { } // For EF Core
 
     public Avatar(Guid userId, Guid avatarId, ETag etag) : base(avatarId)
     {
-        UserId = userId;
+        UserProfileId = userId;
         Etag = etag;
     }
 }
