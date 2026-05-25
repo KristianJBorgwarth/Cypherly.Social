339f56cbd1bb5f3c621872802574010672086ba1
Kristian Borgwarth <10348902@pm.me>
2026-05-25 12:24:41 +0200

    rm profile picture url

---
+++ Social.Infrastructure/Persistence/Migrations/20260525101753_rm_userprofile_picture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Social.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class rm_userprofile_picture : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "profile_picture_url",
+                table: "user_profile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "profile_picture_url",
+                table: "user_profile",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
